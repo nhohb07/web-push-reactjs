c4e00abb0b7a73b3969e7fa026e2d7cc68af33bb
Nho Huynh <nhohb@devblock.net>
2024-03-16 14:52:43 +0700

    Remove check service worker to test on Safari

--- src/hooks/useNotification.js
+++ src/hooks/useNotification.js
@@ -1,18 +1,18 @@
 import { useEffect } from "react";
 
-function checkServiceWorkerAvailable() {
-  if (!("serviceWorker" in navigator)) {
-    throw new Error("Your browser doesn't support service worker!");
-  }
+// function checkServiceWorkerAvailable() {
+//   if (!("serviceWorker" in navigator)) {
+//     throw new Error("Your browser doesn't support service worker!");
+//   }
 
-  if (!("Notification" in window)) {
-    throw new Error("Your browser doesn't support for show notification!");
-  }
+//   if (!("Notification" in window)) {
+//     throw new Error("Your browser doesn't support for show notification!");
+//   }
 
-  if (!("PushManager" in window)) {
-    throw new Error("Your browser doesn't support for push notificaiton API!");
-  }
-}
+//   if (!("PushManager" in window)) {
+//     throw new Error("Your browser doesn't support for push notificaiton API!");
+//   }
+// }
 
 async function requestNotificationPermission() {
   if (Notification.permission === "granted") {
@@ -32,7 +32,6 @@ async function registerServiceWorker() {
 export const useNotification = () => {
   useEffect(() => {
     try {
-      checkServiceWorkerAvailable();
       requestNotificationPermission().then(() => registerServiceWorker());
     } catch (error) {
       alert(error.message);
